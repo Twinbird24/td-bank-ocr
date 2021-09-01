d4a071944d69542d312f3763ac5d413e6b6e690c
Attila Komaromi <attila.kom@gmail.com>
2021-08-31 20:19:01 -0400

    decode and write file with single account number entry

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-const { readFile } = require('fs/promises')
+const { readFile, writeFile } = require('fs/promises')
 const entryArrayToCells = require('./utils/entryArrayToCells')
 const convertDigitToSingleNumber = require('./utils/convertDigitToSingleNumber')
 
 const OcrNumberReader = async (file) => {
+  const outputFile = '/Users/attila/Documents/projects/test-double/td-bank-ocr/src/output/account-numbers.txt'
+
   const fileContents = await readFile(file, { encoding: "utf8" })
   const fileContentArray = fileContents.toString().split('\n')
   const digitCells = entryArrayToCells(fileContentArray)
-  const numbers = digitCells.map(digit => convertDigitToSingleNumber(digit))
+  const numbersArr = digitCells.map(digit => convertDigitToSingleNumber(digit))
+  const numbers = numbersArr.reduce((acc, curr) => acc + curr)
 
-  return numberCells
+  await writeFile(outputFile, numbers, { encoding: "utf8" });
 }
 
 module.exports = OcrNumberReader;
--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -1,10 +1,14 @@
 const OcrNumberReader = require('../index.js')
+const { readFile } = require('fs/promises')
 
 it('reads a file of entires and outputs a file with a list of numbers', async () => {
   const singleEntry = '/Users/attila/Documents/projects/test-double/td-bank-ocr/src/examples/single-entry.txt'
+  const outputFile = '/Users/attila/Documents/projects/test-double/td-bank-ocr/src/output/account-numbers.txt'
   const expectedOutput = '123456789'
 
-  const fileContents = await OcrNumberReader(singleEntry)
+  await OcrNumberReader(singleEntry)
+
+  const fileContents = await readFile(outputFile, { encoding: "utf8" })
 
   expect(fileContents).toBe(expectedOutput)
 })
