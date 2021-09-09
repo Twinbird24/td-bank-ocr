e5a08f4405056724fa9efe24c9e87301caa1f8b1
Attila Komaromi <attila.kom@gmail.com>
2021-09-09 16:35:50 -0400

    handle multi-entry input file

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ const path = require('path');
 const entryArrayToCells = require('./utils/entryArrayToCells')
 const convertDigitToSingleNumber = require('./utils/convertDigitToSingleNumber')
 
+const convertEntryIntoNumbers = (entryArray) => {
+  const digitCells = entryArrayToCells(entryArray)
+  const numbersArr = digitCells.map(convertDigitToSingleNumber)
+  return numbersArr.reduce((acc, curr) => acc + curr)
+}
+
 const OcrNumberReader = async (file) => {
   const outputFile = path.join(__dirname + '/output/account-numbers.txt')
+  const entryFile = await readFile(file, { encoding: "utf8" })
+  const entiresArrayByLine = entryFile.toString().split('\n')
+  const entryLineHeight = 4
+  let numbers = ''
 
-  const fileContents = await readFile(file, { encoding: "utf8" })
-  const fileContentArray = fileContents.toString().split('\n')
-  const digitCells = entryArrayToCells(fileContentArray)
-  const numbersArr = digitCells.map(digit => convertDigitToSingleNumber(digit))
-  const numbers = numbersArr.reduce((acc, curr) => acc + curr)
+  for (let i = 0; i < entiresArrayByLine.length; i+=entryLineHeight) {
+    const number = convertEntryIntoNumbers(entiresArrayByLine.slice(i, i+entryLineHeight))
+    numbers += `${number}\n`
+  }
 
-  await writeFile(outputFile, numbers, { encoding: "utf8" });
+  await writeFile(outputFile, numbers.trim(), { encoding: "utf8" });
 }
 
 module.exports = OcrNumberReader;
--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -2,12 +2,34 @@ const OcrNumberReader = require('../index.js')
 const { readFile } = require('fs/promises')
 const path = require('path');
 
-it('reads a file of entires and outputs a file with a list of numbers', async () => {
+it('reads a single-entry file and outputs a file with with the correct number', async () => {
   const singleEntry = path.join(__dirname + '/../examples/single-entry.txt')
   const outputFile = path.join(__dirname + '/../output/account-numbers.txt')
   const expectedOutput = '123456789'
 
   await OcrNumberReader(singleEntry)
+
+  const fileContents = await readFile(outputFile, { encoding: "utf8" })
+
+  expect(fileContents).toBe(expectedOutput)
+})
+
+it('reads a file of entires and outputs a file with a list of numbers', async () => {
+  const useCase1 = path.join(__dirname + '/../examples/use-case-1.txt')
+  const outputFile = path.join(__dirname + '/../output/account-numbers.txt')
+  const expectedOutput = `000000000
+111111111
+222222222
+333333333
+444444444
+555555555
+666666666
+777777777
+888888888
+999999999
+123456789`
+
+  await OcrNumberReader(useCase1)
 
   const fileContents = await readFile(outputFile, { encoding: "utf8" })
 
